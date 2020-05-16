0858e2bb0f9e171f550432d18b46c341c3fe6d4f
timafon <zvyaginartems@gmail.com>
2020-05-16 22:56:53 +0300

    setup eslint

--- components/app/index.ts
+++ components/app/index.ts
@@ -13,7 +13,7 @@ const app = express()
 
 app.get('/', (req: Request, res: Response) => {
 	res.send('Hello World!')
-});
+})
 
 app.get('/user', (req: Request, res: Response) => {
 	res.status(200).json({ name: 'tobi' })
